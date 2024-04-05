83f29bffe01dd866744b1248d9c1cec38dda90da
Sam Poder <sam.r.poder@gmail.com>
2024-04-05 16:29:37 -0700

    Create action_cable_reference.py

---
+++ action_cable_reference.py
@@ -0,0 +1,20 @@
+import aioactioncable
+import asyncio
+import json
+import ssl
+
+def process(msg):
+  msg_json = json.loads(msg)
+  print(f'Processing {msg_json}')
+  # JSON is in this format: {'experiment': 4, 'control': 'dial_2', 'value': '54', 'controlId': 0.73771245889517, 'location': 'pi'}
+
+
+async def ac_recv(uri, identifier):
+  async with aioactioncable.connect(uri) as acconnect:
+    subscription = await acconnect.subscribe(identifier)
+    async for msg in subscription:
+      if json.loads(msg)['location'] == 'pi':
+        process(msg)
+        await subscription.send({**json.loads(msg), 'location': 'controls'})
+
+asyncio.run(ac_recv('ws://127.0.0.1:3000/cable', {'channel': 'ExperimentChannel', 'experiment': '4', "location": 'pi'}))
