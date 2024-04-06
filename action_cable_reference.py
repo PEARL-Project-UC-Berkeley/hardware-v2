54052697691adf5d104de0c7b67f2ffab64a7819
1saacromer <1saacromero@berkeley.edu>
2024-04-05 17:00:42 -0700

    Update action_cable_reference.py

--- action_cable_reference.py
+++ action_cable_reference.py
@@ -3,10 +3,15 @@ import asyncio
 import json
 import ssl
 
+state = {}
 def process(msg):
   msg_json = json.loads(msg)
   print(f'Processing {msg_json}')
   # JSON is in this format: {'experiment': 4, 'control': 'dial_2', 'value': '54', 'controlId': 0.73771245889517, 'location': 'pi'}
+  state[msg['control']] = msg['value']
+
+      
+    
 
 
 async def ac_recv(uri, identifier):
